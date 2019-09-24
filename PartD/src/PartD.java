09d698afa304652c3e218f0bed52a189f72c9703
lia.smith <lia.ariel.smith@gmail.com>
2019-09-23 19:16:11 -0500

    third line

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("My name is Lia Smith");
 		System.out.println("My favourite colour is blue");
+		System.out.println("My favourite mode of transportation is walking");
 
 	}
 
