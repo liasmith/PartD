44122021562f5f1d99960f95c9fb5251b3c0d0fe
lia.smith <lia.ariel.smith@gmail.com>
2019-09-23 19:16:58 -0500

    fourth line

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("My name is Lia Smith");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite mode of transportation is walking");
+		System.out.println("The best day of the week is Saturday");
 
 	}
 
